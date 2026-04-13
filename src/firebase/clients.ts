32baa7a2ec5abb7b4dc692d11ab9af4134a4b808
ivettesanjurjo <ivettesanjurjo@twentic.com>
2026-04-13 23:08:16 +0200

    Onboarding + categories + dashboard + transactions

--- src/firebase/clients.ts
+++ src/firebase/clients.ts
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+console.log("Firebase API Key check:", import.meta.env.PUBLIC_FIREBASE_API_KEY);
 const firebaseConfig = {
   apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
   authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
